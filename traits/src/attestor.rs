aaa1fa399d641044e2ef440e8bc9660195bd3f82
Liao1 <liaoyi1994@gmail.com>
2022-01-31 14:20:18 +0800

    attestor

--- traits/src/attestor.rs
+++ traits/src/attestor.rs
@@ -1,7 +1,13 @@
+use frame_support::dispatch::DispatchResult;
 use primitives::attestor::ReportType;
 
 pub trait ApplicationTrait {
-    fn unhealthy_application(who: Self::AccountId, report_type: ReportType) -> DispatchResult;
+    /// Currently we will only report a simple `unhealthy` state, but there might be more status in the future.
+    /// E.g maybe something wrong with the application binary
+    fn application_unhealthy(who: Self::AccountId) -> DispatchResult;
+
+    /// Application are attested by several attestors, and reach healthy state
+    fn application_healthy(who: Self::AccountId) -> DispatchResult;
 }
 
 pub trait AttestorTrait {
